af7e2b352988133e08d07643193f191188d2f2c1
webnoi <bilal@webnoi.com>
2025-03-16 03:29:49 +0300

    install-hooks.sh güncellendi

--- install-hooks.sh
+++ install-hooks.sh
@@ -2,4 +2,13 @@
 
 set -eo pipefail
 
-ln -sf $PWD/pre-commit-hook.sh .git/hooks/pre-commit
+install_hook() {
+  # shellcheck disable=SC2155
+  local hook_path="$(pwd)/$1-hook.sh"
+
+  if [ -f "$hook_path" ] && [ ! -L ".git/hooks/$1" ]; then
+    ln -sf "$hook_path" ".git/hooks/$1"
+  fi
+}
+
+install_hook "pre-commit"
