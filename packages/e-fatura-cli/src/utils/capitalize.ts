9f67cf046929ed4accdd06f7224dae2f44fe7e30
Bilal Eren <bilal@webnoi.com>
2025-12-07 19:26:56 +0300

    fix(cli/capitalize): lowercase input before capitalizing

--- packages/e-fatura-cli/src/utils/capitalize.ts
+++ packages/e-fatura-cli/src/utils/capitalize.ts
@@ -1,4 +1,5 @@
 function capitalize<T extends string>(value: T): Capitalize<T> {
+  value = value.toLowerCase() as T;
   return (value.charAt(0).toUpperCase() + value.slice(1)) as Capitalize<T>;
 }
 
